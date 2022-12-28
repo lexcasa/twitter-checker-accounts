a2fd5953bc1b8355076085d2b4dc98ed46734f19
Alex Casadevall <lexcasa@MacBook-Air-de-Alex.local>
2022-12-28 08:39:53 -0300

    fix: remove setMaxListeners for memory leak - lets destroy the server

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // super fast browser - like it :)
 require('dotenv').config()
+
 // Add limit event listeners
-require('events').EventEmitter.prototype._maxListeners = 1000;
+try {
+    process.setMaxListeners(0);
+} catch (e){
+    console.log(`Error setMaxListeners :: ${e}`)
+}
+
 const fs        = require('fs')
 const shell     = require('shelljs')
 const readline  = require('readline');
@@ -35,7 +41,7 @@ async function main () {
             // Run resolver
             const resolveAll = await Promise.all(resolver)
             console.log("resolveAll: ", resolveAll)
-            
+
             // Remove pptr dev profiles
             shell.exec('rm -rf /tmp/puppeteer_dev_*')
             console.log("Dev profile removed :: pptr")
